e69e17ab1d74d7b5dd25460fcd799c325d72ee51
julia <julia.semeraro@affectiva.com>
2017-09-20 11:04:17 -0400

    editing controller

--- app/controllers/socify/users/omniauthable_callbacks_controller.rb
+++ app/controllers/socify/users/omniauthable_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class Socify::Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class Socify::User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
       # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = Socify::User.from_omniauth(request.env['omniauth.auth'])
