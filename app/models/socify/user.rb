99c7e49da7ab202c11ff87cc77b26d9c7d97b2db
julia <julia.semeraro@affectiva.com>
2017-09-26 10:55:56 -0400

    Google specific adjustment

--- app/models/socify/user.rb
+++ app/models/socify/user.rb
@@ -34,15 +34,18 @@ module Socify
     extend FriendlyId
     friendly_id :name, use: [:slugged, :finders]
 
-    def self.create_from_provider_data(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do | user |
-        user.email = auth.info.email
-        user.password = Devise.friendly_token[0, 20]
-        user.name = auth.info.name
-        user.avatar = auth.info.image
-        user.sex = auth.info.gender
-        # user.skip_confirmation!
+    def self.from_omniauth(access_token)
+      data = access_token.info
+      user = User.where(email: data['email']).first
+  
+      # Uncomment the section below if you want users to be created if they don't exist
+      unless user
+          user = User.create(name: data['name'],
+             email: data['email'],
+             password: Devise.friendly_token[0,20]
+          )
       end
-    end
+      user
+  end
   end
 end
