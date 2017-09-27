f4bd6c810efcd9eededce509660c9f265e6adcb4
julia <julia.semeraro@affectiva.com>
2017-09-27 14:03:32 -0400

    editing email set up

--- app/models/socify/user.rb
+++ app/models/socify/user.rb
@@ -56,8 +56,12 @@ module Socify
         # Get the existing user by email if the provider gives us a verified email.
         # If no verified email was provided we assign a temporary email and ask the
         # user to verify it on the next step via UsersController.finish_signup
-        email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
-        email = auth.info.email if email_is_verified
+        if auth.provider == 'google_oauth2'
+          email = auth.info.email
+        else
+          email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
+          email = auth.info.email if email_is_verified
+        end
         user = Socify::User.where(:email => email).first if email
   
         # Create the user if it's a new registration
@@ -65,8 +69,7 @@ module Socify
           user = Socify::User.new(
             name: auth.info.name,
             #username: auth.info.nickname || auth.uid,
-            email: email,
-            #  || email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
+            email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
             password: Devise.friendly_token[0,20]
           )
           # user.skip_confirmation!
