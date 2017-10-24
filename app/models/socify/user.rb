7fa7b722298bbfc302b9b1050f716324be192cdf
julia <julia.semeraro@affectiva.com>
2017-10-24 16:31:57 -0400

    fixing for Add Email bug

--- app/models/socify/user.rb
+++ app/models/socify/user.rb
@@ -44,37 +44,30 @@ module Socify
     validates_format_of :email, :without => TEMP_EMAIL_REGEX, on: :update
   
     def self.find_for_oauth(auth, signed_in_resource = nil)
-
-      # Get the identity and user if they exist
       identity = Identity.find_for_oauth(auth)
   
       # If a signed_in_resource is provided it always overrides the existing user
       # to prevent the identity being locked with accidentally created accounts.
-      # Note that this may leave zombie accounts (with no associated identity) which
-      # can be cleaned up at a later date.
       user = signed_in_resource ? signed_in_resource : identity.user
   
-      # Create the user if needed
       if user.nil?
-  
-        # Get the existing user by email if the provider gives us a verified email.
-        # If no verified email was provided we assign a temporary email and ask the
-        # user to verify it on the next step via UsersController.finish_signup
         if auth.provider == 'google_oauth2'
           email = auth.info.email
         else
           email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
-          email = auth.info.email if email_is_verified
+          if email_is_verified
+            email = auth.info.email
+          else 
+            email = "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com"
+          end
         end
 
         user = Socify::User.where(:email => email).first if email
-
-        # Create the user if it's a new registration
         if user.nil?
           user = Socify::User.new(
             name: auth.info.name,
             username: auth.info.nickname || email,
-            email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
+            email: email,
             password: Devise.friendly_token[0,20]
           )
           # user.skip_confirmation!
