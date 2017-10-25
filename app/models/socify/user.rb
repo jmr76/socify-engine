1defcb38360e295eeb7e95346a32310d45533257
julia <julia.semeraro@affectiva.com>
2017-10-25 10:41:15 -0400

    deleting unnecessary comments

--- app/models/socify/user.rb
+++ app/models/socify/user.rb
@@ -44,22 +44,13 @@ module Socify
     validates_format_of :email, :without => TEMP_EMAIL_REGEX, on: :update
   
     def self.find_for_oauth(auth, signed_in_resource = nil)
-
-      # Get the identity and user if they exist
       identity = Identity.find_for_oauth(auth)
   
       # If a signed_in_resource is provided it always overrides the existing user
       # to prevent the identity being locked with accidentally created accounts.
-      # Note that this may leave zombie accounts (with no associated identity) which
-      # can be cleaned up at a later date.
       user = signed_in_resource ? signed_in_resource : identity.user
   
-      # Create the user if needed
       if user.nil?
-  
-        # Get the existing user by email if the provider gives us a verified email.
-        # If no verified email was provided we assign a temporary email and ask the
-        # user to verify it on the next step via UsersController.finish_signup
         if auth.provider == 'google_oauth2'
           email = auth.info.email
         else
@@ -72,8 +63,6 @@ module Socify
         end
 
         user = Socify::User.where(:email => email).first if email
-
-        # Create the user if it's a new registration
         if user.nil?
           user = Socify::User.new(
             name: auth.info.name,
