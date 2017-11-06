ee072b0f18f287168f7b1dde2b3cf03315fec254
Jason Radice <jason.radice@affectiva.com>
2017-11-06 13:35:21 -0500

    Permit username

--- app/controllers/socify/users_controller.rb
+++ app/controllers/socify/users_controller.rb
@@ -64,7 +64,7 @@ module Socify
     private
 
     def user_params
-      params.require(:user).permit(:name, :about, :avatar, :cover, :email,
+      params.require(:user).permit(:name, :username, :about, :avatar, :cover, :email,
                                    :sex, :dob, :location, :phone_number)
     end
 
