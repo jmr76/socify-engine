e2bad64e4483a6f3948aaaff1f23fa890487fd40
julia <julia.semeraro@affectiva.com>
2017-12-15 16:19:03 -0500

    editing path to try to get MiniMagick workig

--- app/uploaders/socify/avatar_uploader.rb
+++ app/uploaders/socify/avatar_uploader.rb
@@ -19,7 +19,7 @@ module Socify
     # Override the directory where uploaded files will be stored.
     # This is a sensible default for uploaders that are meant to be mounted:
     def store_dir
-      "public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      "#{mounted_as}/#{model.id}"
     end
 
     # Provide a default URL as a default if there hasn't been a file uploaded:
