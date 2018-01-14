3ac6d0bad86df113650b19785e908b21ed4ad7f3
Jason Radice <jason.radice@affectiva.com>
2018-01-14 15:28:04 -0500

    More debug code removal

--- app/controllers/socify/users/omniauth_callbacks_controller.rb
+++ app/controllers/socify/users/omniauth_callbacks_controller.rb
@@ -15,26 +15,11 @@ class Socify::Users::OmniauthCallbacksController < Devise::OmniauthCallbacksCont
     }
   end
 
-  # def facebook
-  #   byebug
-  #   @user = Socify::User.find_for_oauth(request.env['omniauth.auth'].except(:extra), current_user)
-  # 
-  #   if @user.persisted?
-  #     sign_in_and_redirect @user, event: :authentication
-  #     set_flash_message(:notice, :success, kind: "facebook".capitalize) if is_navigational_format?
-  #   else
-  #     session["devise.facebook_data"] = request.env['omniauth.auth'].except(:extra)
-  #     redirect_to new_user_registration_url
-  #   end
-  # end
-
-  # :facebook, 
   [:twitter, :google_oauth2, :facebook].each do |provider|
     provides_callback_for provider
   end
 
   def after_sign_in_path_for(resource)
-    byebug
     if resource.email_verified?
       if @user.created_at == @user.last_sign_in_at && @user.created_at == @user.current_sign_in_at && @user.last_sign_in_at == @user.current_sign_in_at
         main_app.install_extension_path
--- config/initializers/facebook_token_authenticable_initializer.rb
+++ config/initializers/facebook_token_authenticable_initializer.rb
@@ -1,5 +1,2 @@
-puts "FB INITIALIZER"
-#require 'devise/strategies/facebook_token_authenticatable'
-
 require 'facebook_token_authenticatable'
 
